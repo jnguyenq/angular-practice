073c3707033880c29b6eb883469e66d7c8cba950
jnguyenq <jnguyenq@gmu.edu>
2016-01-10 10:21:07 -0500

    formatting

--- controllers/home.js
+++ controllers/home.js
@@ -8,27 +8,27 @@ var knex = require('../models/database').knex;
 
 module.exports = function (req, res) {
 	
-	var rawCookie = req.headers.cookie;
+var rawCookie = req.headers.cookie;
 
-	if(rawCookie) {
-		var processedCookie = rawCookie.split("=");
-		var cookie = processedCookie[1];
-		var sessionIdArray = [];
-		knex.select('session_id')
-			.from('friends')
-			.then(function(data) {
-				data.forEach(function(item) {
-					sessionIdArray.push(item.session_id);
-				})
-				if((sessionIdArray).indexOf(cookie) > -1) {
-					console.log('exists');
-					res.render('home.html')
-				} else {
-					res.redirect('/login');
-				}
-			});
-	} else {
-		res.redirect('/login');
-	}
+if(rawCookie) {
+	var processedCookie = rawCookie.split("=");
+	var cookie = processedCookie[1];
+	var sessionIdArray = [];
+	knex.select('session_id')
+		.from('friends')
+		.then(function(data) {
+			data.forEach(function(item) {
+				sessionIdArray.push(item.session_id);
+			})
+			if((sessionIdArray).indexOf(cookie) > -1) {
+				console.log('exists');
+				res.render('home.html')
+			} else {
+				res.redirect('/login');
+			}
+		});
+} else {
+	res.redirect('/login');
+}
 
 };
